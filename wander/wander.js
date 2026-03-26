065e2e11726914dce820d338d8574f580996bb8c
Kai Kühne <github@kaikuehne.net>
2026-03-26 01:44:53 +0100

    Add favicon

--- wander/wander.js
+++ wander/wander.js
@@ -1,19 +1,11 @@
 const wander = {
-  consoles: [
-    // 'https://susam.net/wander/',
-  ],
-
+  consoles: [],
+  ignore: [],
   pages: [
       'https://xnacly.me/',
       'https://ciechanow.ski/',
       'https://blog.avas.space/',
       'https://jvns.ca/',
-  ],
-
-  ignore: [
-    '.*://medium\\.com/.*',
-    '.*://.*\\.substack\\.com/.*',
-    '.*://cari\\.institute/.*',
-    '.*://wdl\\.mcdaniel\\.edu/.*',
+      'https://nullprogram.com/',
   ]
 }
