49d12022d1a579d7cf94a028470a87c0be12fc46
Kai Kühne <github@kaikuehne.net>
2026-04-22 01:49:42 +0200

    Update wander to 0.5.0

--- wander/wander.js
+++ wander/wander.js
@@ -1,11 +1,13 @@
 const wander = {
-  consoles: [],
-  ignore: [],
-  pages: [
-      'https://xnacly.me/',
-      'https://ciechanow.ski/',
-      'https://blog.avas.space/',
-      'https://jvns.ca/',
-      'https://nullprogram.com/',
-  ]
+    consoles: [],
+    ignore: [],
+    pages: [
+        'https://xnacly.me/',
+        'https://ciechanow.ski/',
+        'https://blog.avas.space/',
+        'https://jvns.ca/',
+        'https://nullprogram.com/',
+        'https://everynoise.com/engenremap.html',
+        'https://film-grab.com/',
+    ]
 }
